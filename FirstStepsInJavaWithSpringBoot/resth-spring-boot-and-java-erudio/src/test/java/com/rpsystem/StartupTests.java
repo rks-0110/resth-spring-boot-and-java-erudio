5b00c51f5a3988484aac7ec1f86c7cfa5a527d42
Hiroshi Shihomatsu <lhshihomatsu.trabalho@gmail.com>
2024-08-01 17:05:51 -0300

    First Steps in Java with Spring Boot 3

---
+++ FirstStepsInJavaWithSpringBoot/resth-spring-boot-and-java-erudio/src/test/java/com/rpsystem/StartupTests.java
@@ -0,0 +1,13 @@
+package com.rpsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
