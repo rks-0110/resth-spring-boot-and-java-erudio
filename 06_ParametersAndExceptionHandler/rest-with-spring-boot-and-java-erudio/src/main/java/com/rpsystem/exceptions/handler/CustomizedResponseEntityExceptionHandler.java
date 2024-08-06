9a9cac9268a4be769c843f1ce3ad2db222e2d0dd
Hiroshi Shihomatsu <lhshihomatsu.trabalho@gmail.com>
2024-08-06 12:19:13 -0300

    sessao06

---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/com/rpsystem/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.rpsystem.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.rpsystem.exceptions.ExceptionResponse;
+import com.rpsystem.exceptions.UnsupportedMathOperationException;
+
+@ControllerAdvice // sempre que precisar concentrar os controles, para não ter que criar diversos controles
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllException(
+			Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(), 
+				ex.getMessage(), 
+				request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(UnsupportedMathOperationException.class)
+	public final ResponseEntity<ExceptionResponse> handleBadRequestException(
+			Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(), 
+				ex.getMessage(), 
+				request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+
+}
